75cae77b09a57b5d55476ce1597099855bffd5cc
RobertGollagher <robert.gollagher@freeputer.net>
2016-03-29 22:15:48 +1000

    Added fvmtest for alignment
    Also found that Arduino Due does not need FVMO_SAFE_ALIGNMENT

--- pre-alpha/pre-alpha1.1/dev/fvm/c/src/fvm.c
+++ pre-alpha/pre-alpha1.1/dev/fvm/c/src/fvm.c
@@ -6,8 +6,8 @@ Program:    fvm.c
 Copyright © Robert Gollagher 2015, 2016
 Author :    Robert Gollagher   robert.gollagher@freeputer.net
 Created:    20150822
-Updated:    20160329:2039
-Version:    pre-alpha-0.0.0.26 for FVM 1.1
+Updated:    20160329:2208
+Version:    pre-alpha-0.0.0.27 for FVM 1.1
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -535,6 +535,7 @@ IMPORTANT WARNINGS REGARDING THIS 'fvm.c' MULTIPLEXING IMPLEMENTATION:
   #define FVMO_TRON
   #define FVMO_SEPARATE_ROM
   #define FVMO_INCORPORATE_ROM
+//  #define FVMO_SAFE_ALIGNMENT
 #endif
 
 /* Generic option set: typical Arduino Uno options */
