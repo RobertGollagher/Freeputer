8c5e0cfa248727b671d2a6861e55ce2f16d2231a
RobertGollagher <robert.gollagher@freeputer.net>
2017-05-13 18:23:55 +1000

    Use strict

--- pre-alpha/pre-alpha2.0/dev/fvm/js/fvm2.js
+++ pre-alpha/pre-alpha2.0/dev/fvm/js/fvm2.js
@@ -5,8 +5,8 @@
  * Program:    fvm2.js
  * Author :    Robert Gollagher   robert.gollagher@freeputer.net
  * Created:    20170303
- * Updated:    20170513-1704
- * Version:    pre-alpha-0.0.0.15 for FVM 2.0
+ * Updated:    20170513-1720
+ * Version:    pre-alpha-0.0.0.16 for FVM 2.0
  *
  *                               This Edition:
  *                                JavaScript 
