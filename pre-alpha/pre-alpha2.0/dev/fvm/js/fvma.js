ba99b2cce06226968581347345fe2f142321ac3d
RobertGollagher <robert.gollagher@freeputer.net>
2017-06-11 21:11:01 +1000

    Progress on assembler

--- pre-alpha/pre-alpha2.0/dev/fvm/js/fvma.js
+++ pre-alpha/pre-alpha2.0/dev/fvm/js/fvma.js
@@ -6,7 +6,7 @@
  * Author :    Robert Gollagher   robert.gollagher@freeputer.net
  * Created:    20170611
  * Updated:    20170611-1431+
- * Version:    pre-alpha-0.0.0.0 for FVM 2.0
+ * Version:    pre-alpha-0.0.0.1 for FVM 2.0
  *
  *                     This Edition of the Assembler:
  *                                JavaScript
@@ -30,11 +30,67 @@ var modFVMA = (function () { 'use strict';
   class FVMA {
     constructor(fnMsg) {
       this.fnMsg = fnMsg;
+      this.prgElems = new PrgElems();
     };
 
-    asm() {
-      this.fnMsg('Assembler not yet implemented');
+    asm(str) { // FIXME no enforcement yet
+      this.fnMsg('Parsing...');
+      var lines = str.split(/\n/);
+      try {
+        for (var i = 0; i < lines.length; i++) {
+          this.parseLine(lines[i], i+1);
+        }
+        this.fnMsg(this.prgElems);
+        this.fnMsg('Melding...');
+        this.prgElems.meld();
+        this.fnMsg(this.prgElems);
+        this.fnMsg('\nTODO: implement use of assembled program');
+      } catch (e) {
+        this.fnMsg(e);
+      }
     };
+
+    parseLine(line, lineNum) {
+      // Tokens are separated by any whitespace. Tokens contain no whitespace.
+      var tokens = line.split(/\s+/).filter(x => x.match(/\S+/));
+      tokens.forEach(token => this.parseToken(token,lineNum), this);
+    }
+
+    parseToken(token, lineNum) {
+      if ((token.length === 8 || token.length === 4) && token.match(/0x[0-9a-f]{2,6}/)) {
+        var n = parseInt(token,16);
+        this.prgElems.addElem(n);
+      } else {
+        throw lineNum + ":Syntax error:" + token;
+      }
+    };
+
+  };
+
+  class PrgElems {
+    constructor() {
+      this.cursor = 0;
+      this.elems = [];
+    };
+
+    addElem(n) {
+      this.cursor = this.elems.push(n);
+    }
+
+    meld() {
+      var melded = [];
+      for (var i = 0; i < this.elems.length-1; i=i+2) {
+        melded.push(this.elems[i] + (this.elems[i+1] << 8));
+      }
+      this.elems = melded;
+    }
+
+    toString() {
+      var str = ":";
+      this.elems.forEach(x => str = str + modFmt.hex8(x) + ":");
+      return str;
+    }
+
   };
 
   return {
