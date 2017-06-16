ce7fda470b86a5f7858736f778e8559657dd5455
RobertGollagher <robert.gollagher@freeputer.net>
2017-06-17 09:26:51 +1000

    Experiment without phrases

--- pre-alpha/pre-alpha2.0/dev/fvm/js/prg.js
+++ pre-alpha/pre-alpha2.0/dev/fvm/js/prg.js
@@ -1,84 +1,37 @@
-var prgSrc = `///startProgram jmp /handleFailedProgram 0p000003 lit 0x000001
-frt ---
-cal /put2 0p000001
-frt ---
-hal ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-///put2 lit 0x000002
-frt ---
-cal /put3 0p000002
-frt ---
-ret 0f000001
-fal ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-///put3 lit 0x000003
-frt ---
-frt ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-///handleFailedProgram lit 0x0000ff
-fal ---
-hal ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-(
-/// nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-nop ---
-)
+var prgSrc = `fal --- jmp /start 0x00000e
+/put1 lit 0x000001
+  frt ---
+  ret ---
+/put2and2 /replaced jmp 0x000017
+  fal ---
+  fal ---
+/put4 lit 0x000004
+  frt ---
+  ret ---
+/put3 lit 0x000003
+  frt ---
+  ret ---
+/put1to4 cal /put1 0x000002
+  frt ---
+  cal /put2and2 0x000005
+  frt ---
+  cal /put3 0x00000b
+  frt ---
+  cal /put4 0x000008
+  frt ---
+  hal ---
+/put2and2 lit 0x000002
+  frt ---
+  lit 0x000002
+  frt ---
+  ret ---
+/start cal /put1to4 0x00000e ( =========================== )
+  frt ---
+  hal ---
+
+
+
+
 ( Copyright 2017, Robert Gollagher.
   SPDX-License-Identifier: GPL-3.0+ )
 `;
