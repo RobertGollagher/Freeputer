0512f165abea14952dcd55632219b44c2533680b
RobertGollagher <robert.gollagher@freeputer.net>
2017-06-17 12:34:57 +1000

    prg.js refactoring shows #def is beneficial and necessary

--- pre-alpha/pre-alpha2.0/dev/fvm/js/prg.js
+++ pre-alpha/pre-alpha2.0/dev/fvm/js/prg.js
@@ -1,16 +1,19 @@
-var prgSrc = `fal --- jmp 0x000010
+var prgSrc = `fal --- jmp 0x000013 ( need a forward ref here )
 #def /put1 0s000001 . lit 0x000001
   frt ---
   ret ---
-#def /put2and2 0s000002 . lit 0x000002      ( a fixed bug )
+#def /put2 0s000002 . lit 0x000002
   frt ---
-  lit 0x000002
+  ret ---
+#def /put3 0s000004 . lit 0x000003 ( moved )
   frt ---
   ret ---
 #def /put4 0s000003 . lit 0x000004
   frt ---
   ret ---
-#def /put3 0s000004 . lit 0x000003
+#def /put2and2 0s000006 . cal /put2 0s000002 ( refactored )
+  frt ---
+  cal /put2 0s000002
   frt ---
   ret ---
 #def /Molecule 0s000005 . cal /put4 0s000003
