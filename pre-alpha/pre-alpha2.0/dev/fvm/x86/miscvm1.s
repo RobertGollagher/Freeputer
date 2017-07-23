7a06f5cc84090f524ff19d1d6f8a2cee3e7e0c34
RobertGollagher <robert.gollagher@freeputer.net>
2017-07-23 18:58:36 +1000

    Fixed .lcomm and .equ and added esp use.

--- pre-alpha/pre-alpha2.0/dev/fvm/x86/miscvm1.s
+++ pre-alpha/pre-alpha2.0/dev/fvm/x86/miscvm1.s
@@ -8,7 +8,7 @@ Program:    miscvm1
 Author :    Robert Gollagher   robert.gollagher@freeputer.net
 Created:    20170723
 Updated:    20170723+
-Version:    pre-alpha-0.0.0.2+ for FVM 2.0
+Version:    pre-alpha-0.0.0.3+ for FVM 2.0
 
 Notes: This is a MISC experiment which takes 'tvm.s' as its starting point
 and attempts to further simplify that by another order of magnitude.
@@ -110,38 +110,67 @@ Alternative if no imports:
 .equ vL, %edx # was %eax # link register # FIXME maybe remove this
 .equ vS, %esi # source address register
 .equ vD, %edi # destination address register
+.equ vP, %esp # stack pointer
 # Registers of the implementation:
 .equ rTmp, %ecx # temporary register (must be %ecx because of shl, shr)
 
 # ============================================================================
 # ============================================================================
 .macro OUTCHAR reg
+  do_save_vm_sp
+  do_restore_sys_sp
   pushl \reg
   call putchar
   addl $WORD_SIZE, %esp
+  do_save_sys_sp
+  do_restore_vm_sp
 .endm
 
 .macro INCHAR #FIXME
+  do_save_vm_sp
+  do_restore_sys_sp
   call getchar
   movl %eax, vA
+  do_save_sys_sp
+  do_restore_vm_sp
+.endm
+
+.macro do_save_sys_sp
+  movl %esp, sys_sp
+.endm
+
+.macro do_restore_sys_sp
+  movl sys_sp, %esp
+.endm
+
+.macro do_save_vm_sp
+  movl %esp, vm_sp
+.endm
+
+.macro do_restore_vm_sp
+  movl vm_sp, %esp
 .endm
 
 .macro do_failure
+  do_restore_sys_sp
   movl $FAILURE, rTmp
   ret
 .endm
 
 .macro do_success
+  do_restore_sys_sp
   movl $SUCCESS, rTmp
   ret
 .endm
 
 .macro do_init
+  do_save_sys_sp
   xorl vA, vA
   xorl vB, vB
   xorl vL, vL
   xorl vD, vD
   xorl vS, vS
+  xorl vP, vP
   xorl rTmp, rTmp
 .endm
 
@@ -149,23 +178,39 @@ Alternative if no imports:
 .section .data #             INSTRUCTION SET  #FIXME maybe go 32 not 24?
 # ============================================================================
 .macro lit x
+  movl $\x, vA
+.endm
+
+.macro op x
   movl $\x, vB
 .endm
+
+.macro src x
+  movl $\x, vS
+.endm
+
+.macro dst x
+  movl $\x, vD
+.endm
+
+.macro link x
+  movl $\x, vD
+.endm
 # ----------------------------------------------------------------------------
-.macro load # replaces from x
+.macro load
   movl data_memory(,vS,1), vA
 .endm
 
-.macro store # replaces to x
+.macro store
   movl vA, data_memory(,vD,1)
 .endm
 
-.macro pull # replaces from_ptr x
+.macro pull
   movl data_memory(,vS,1), rTmp
   movl data_memory(,rTmp,1), vA
 .endm
 
-.macro put # replaces to_ptr x
+.macro put
   movl data_memory(,vD,1), rTmp
   movl vA, data_memory(,rTmp,1)
 .endm
@@ -185,6 +230,15 @@ Alternative if no imports:
 .macro incd
   addl $WORD_SIZE, vS
 .endm
+
+.macro push
+  pushl vA
+.endm
+
+.macro pop
+  popl vA
+.endm
+
 # ----------------------------------------------------------------------------
 .macro add
   addl vB, vA
@@ -316,7 +370,7 @@ Alternative if no imports:
 .macro movsa
   movl vS, vA
 .endm
-
+# ----------------------------------------------------------------------------
 .macro halt
   movl vB, %eax
   jmp vm_success
@@ -325,7 +379,9 @@ Alternative if no imports:
 # ============================================================================
 .section .bss #                  VARIABLES
 # ============================================================================
-data_memory: .lcomm mm, DM_BYTES
+.lcomm data_memory, DM_BYTES
+.lcomm sys_sp, WORD_SIZE
+.lcomm vm_sp, WORD_SIZE
 
 # ============================================================================
 .section .text #             EXIT POINTS for the VM
@@ -340,24 +396,71 @@ vm_success:
 
 # ============================================================================
 # ========================= EXAMPLE PROGRAM ==================================
+#         The example shall virtualize this VM within itself!
+#     Labels starting with vm_ are used by the parent native VM.
+#     Labels starting with v_ are used by the child virtualized VM.
+#     The child shall use ???-wide instructions (two formats).
 # ============================================================================
+.equ v_data_memory, 0
+.equ v_DM_BYTES, 0x100000 # Smaller than parent DM_BYTES by arbitrary amount
+.equ v_rPC, v_data_memory + v_DM_BYTES # Note: parent has no explicit rPC
+.equ v_vA, v_rPC + WORD_SIZE
+.equ v_vB, v_vA + WORD_SIZE
+.equ v_vL, v_vB + WORD_SIZE
+.equ v_vS, v_vL + WORD_SIZE
+.equ v_vD, v_vS + WORD_SIZE
+# Just using arbitrary opcode designations for now:
+.equ v_LIT,   0x01000000
+.equ v_HALT,  0x1f000000
 
 # ============================================================================
-#                             ENTRY POINT
+#                     ENTRY POINT
 # ============================================================================
 .global main
 main:
+vm_init: # parent
   do_init
 
-  lit 0x7fffffff
-  movba
+vm_load_program_for_child:
+  dst 0
 
-  loop: # 1.4 seconds
-    lit 1
-    sub
-    jmpgz loop
+  lit v_LIT + 0x00123456
+  store
+  incd
 
+  lit v_HALT + 0x0
+  store
+  incd
+
+v_init:  # child
+  lit 0
+
+  dst v_rPC
+  store
+
+  dst v_vA
+  store
+
+  dst v_vB
+  store
+
+  dst v_vL
+  store
+
+  dst v_vS
+  store
+
+  lit v_vD
+  movbd
+  store
+
+end:
   halt
+
+to:
+  movbd
+  store
+  lret
 
 # ============================================================================
 
