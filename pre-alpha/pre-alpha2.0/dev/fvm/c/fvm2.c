65de807d67a4620e3d98dfff6d28d9d0a30b66be
RobertGollagher <robert.gollagher@freeputer.net>
2018-05-02 03:08:27 +1000

    C implementation: added native tracing.

--- pre-alpha/pre-alpha2.0/dev/fvm/c/fvm2.c
+++ pre-alpha/pre-alpha2.0/dev/fvm/c/fvm2.c
@@ -6,7 +6,7 @@ Program:    fvm2.c
 Author :    Robert Gollagher   robert.gollagher@freeputer.net
 Created:    20170729
 Updated:    20180501+
-Version:    pre-alpha-0.0.8.4+ for FVM 2.0
+Version:    pre-alpha-0.0.8.5+ for FVM 2.0
 =======
 
                               This Edition:
@@ -39,6 +39,18 @@ Version:    pre-alpha-0.0.8.4+ for FVM 2.0
 #include <setjmp.h>
 
 // ---------------------------------------------------------------------------
+// Tracing
+// ---------------------------------------------------------------------------
+#define TRACING_SUPPORTED // Uncomment this line to support tracing
+#ifdef TRACING_SUPPORTED
+  #define TRC(mnem) if (fvm.tracing) { __label__ ip; ip: \
+    fprintf(stdtrcHandle, "*%08x %s / %s / ( %s ) [ %s ] { %s } \n", \
+      &&ip, mnem, wsTrace(&fvm.cs), wsTrace(&fvm.ds), wsTrace(&fvm.ts), nsTrace(&fvm.rs)); }
+#else
+  #define TRC(mnem)
+#endif
+
+// ---------------------------------------------------------------------------
 // Platform-specific constants
 // ---------------------------------------------------------------------------
 #define nopasm "nop"  // The name of the native hardware nop instruction
@@ -51,6 +63,10 @@ Version:    pre-alpha-0.0.8.4+ for FVM 2.0
 //     head -c 1024 /dev/zero > std.hld
 //     head -c 1024 /dev/zero > std.rom
 // ---------------------------------------------------------------------------
+#ifdef TRACING_SUPPORTED
+  #define stdtrcFilename "std.trc"
+  FILE *stdtrcHandle;
+#endif
 #define rmFilename "std.rom"
 FILE *rmHandle;
 #define stdhldFilename "std.hld"
@@ -68,6 +84,8 @@ FILE *stdhldHandle;
 #define SHIFT_MASK    0x0000001f
 #define SUCCESS 0
 #define FAILURE 1
+#define FALSE 0
+#define TRUE 1
 #define STACK_CAPACITY 256
 #define STACK_MAX_INDEX STACK_CAPACITY-1
 #define MAX_DM_WORDS 0x10000000 // <= 2^28 due to C limitations.
@@ -105,10 +123,16 @@ WORD hdsafe(WORD addr)       { return addr & HD_MASK; }
 typedef struct WordStack {
   BYTE sp;
   WORD elem[STACK_CAPACITY];
+  #ifdef TRACING_SUPPORTED
+    char traceBuf[20];
+  #endif
 } WDSTACK;
 typedef struct NativeStack {
   BYTE sp;
   NAT elem[STACK_CAPACITY];
+  #ifdef TRACING_SUPPORTED
+    char traceBuf[20];
+  #endif
 } NATSTACK;
 // ---------------------------------------------------------------------------
 void wdPush(WORD x, WDSTACK *s) {
@@ -141,6 +165,14 @@ WORD wdPeek(WDSTACK *s) {
     longjmp(exc_env, FAILURE);
   }
 }
+WORD wdPeekAt(WDSTACK *s, WORD index) {
+  if (index > 0 && (s->sp)>=index) {
+    WORD wd = s->elem[index-1];
+    return wd;
+  } else {
+    longjmp(exc_env, FAILURE);
+  }
+}
 WORD wdPeekAndDec(WDSTACK *s) {
   if ((s->sp)>0) {
     WORD wd = s->elem[(s->sp)-1];
@@ -156,6 +188,17 @@ WORD wdPeekAndDec(WDSTACK *s) {
 }
 int wdElems(WDSTACK *s) {return (s->sp);}
 int wdFree(WDSTACK *s) {return STACK_MAX_INDEX - (s->sp);}
+#ifdef TRACING_SUPPORTED
+  char* wsTrace(WDSTACK *s) {
+    if (wdElems(s) == 0) {
+      return "";
+    } else { // FIXME TODO more elems
+      sprintf(s->traceBuf, "%08x...",wdPeekAt(s,1));
+      return s->traceBuf;
+    }
+  }
+#endif
+
 // ---------------------------------------------------------------------------
 WORD natPush(NAT x, NATSTACK *s) {
   if ((s->sp) < STACK_MAX_INDEX) {
@@ -173,8 +216,26 @@ NAT natPop(NATSTACK *s) {
     longjmp(exc_env, FAILURE);
   }
 }
+WORD nsPeekAt(NATSTACK *s, WORD index) {
+  if (index > 0 && (s->sp)>=index) {
+    NAT nat = s->elem[index-1];
+    return nat;
+  } else {
+    longjmp(exc_env, FAILURE);
+  }
+}
 WORD natElems(NATSTACK *s) {return (s->sp);}
 WORD natFree(NATSTACK *s) {return STACK_MAX_INDEX - (s->sp);}
+#ifdef TRACING_SUPPORTED
+  char* nsTrace(NATSTACK *s) {
+    if (natElems(s) == 0) {
+      return "";
+    } else { // FIXME TODO more elems
+      sprintf(s->traceBuf, "*%08x...",nsPeekAt(s,1));
+      return s->traceBuf;
+    }
+  }
+#endif
 
 // ---------------------------------------------------------------------------
 // FVM structure -- other structures can be logically equivalent
@@ -187,14 +248,12 @@ typedef struct Fvm {
   WDSTACK ts; // temporary stack
   WDSTACK cs; // counter stack
   NATSTACK rs; // return stack
+  #ifdef TRACING_SUPPORTED
+    WORD tracing;
+  #endif
 } FVM;
-FVM fvm; // One global instance only (for program source-code portability)
-
-// ---------------------------------------------------------------------------
-// Utilities
-// ---------------------------------------------------------------------------
-void Dbg()  { ; }
-#define dbg Dbg()
+// One global instance only (for program source-code portability)
+FVM fvm;
 
 // ---------------------------------------------------------------------------
 // Instruction set
@@ -203,44 +262,44 @@ void Noop()    { __asm(nopasm); }
 // Call
 // Ret
 // Rpt
-void Cpush()  { wdPush(wdPop(&fvm.ds),&fvm.cs); }
-void Cpop()   { wdPush(wdPop(&fvm.cs),&fvm.ds); }
-void Cpeek()  { wdPush(wdPeek(&fvm.cs),&fvm.ds); }
-void Cdrop()  { wdDrop(&fvm.cs); }
-void Tpush()  { wdPush(wdPop(&fvm.ds),&fvm.ts); }
-void Tpop()   { wdPush(wdPop(&fvm.ts),&fvm.ds); }
-void Tpeek()  { wdPush(wdPeek(&fvm.ts),&fvm.ds); }
-void Tpoke()  { ; }
-void Tdrop()  { wdDrop(&fvm.ts); }
-void Lit(WORD x)    { wdPush(x&METADATA_MASK,&fvm.ds); }
-void Drop()   { wdDrop(&fvm.ds); }
-void Swap()   { ; }
-void Over()   { ; }
-void Rot()    { ; }
-void Dup()    { ; }
-void Get()    { ; }
-void Put()    { ; }
-void Geti()   { ; }
-void Puti()   { ; }
-void Rom()    { ; }
-void Add()    { wdPush(wdPop(&fvm.ds)+wdPop(&fvm.ds),&fvm.ds); } // FIXME
-void Sub()    { ; }
-void Mul()    { ; }
-void Div()    { ; }
-void Mod()    { ; }
-void Inc()    { ; }
-void Dec()    { ; }
-void Or()     { ; }
-void And()    { ; }
-void Xor()    { ; }
-void Flip()   { ; }
-void Neg()    { ; }
-void Shl()    { ; }
-void Shr()    { ; }
-void Hold()   { ; }
-void Give()   { ; }
-void In()     { wdPush(getchar(), &fvm.ds); }
-void Out()    { putchar(wdPop(&fvm.ds)); }
+void Cpush()  { TRC("cpush") wdPush(wdPop(&fvm.ds),&fvm.cs); }
+void Cpop()   { TRC("cpop ") wdPush(wdPop(&fvm.cs),&fvm.ds); }
+void Cpeek()  { TRC("cpeek") wdPush(wdPeek(&fvm.cs),&fvm.ds); }
+void Cdrop()  { TRC("cdrop") wdDrop(&fvm.cs); }
+void Tpush()  { TRC("tpush") wdPush(wdPop(&fvm.ds),&fvm.ts); }
+void Tpop()   { TRC("tpop ") wdPush(wdPop(&fvm.ts),&fvm.ds); }
+void Tpeek()  { TRC("tpeek") wdPush(wdPeek(&fvm.ts),&fvm.ds); }
+void Tpoke()  { TRC("tpoke") ; }
+void Tdrop()  { TRC("tdrop") wdDrop(&fvm.ts); }
+void Lit(WORD x) { TRC("lit  ") wdPush(x&METADATA_MASK,&fvm.ds); }
+void Drop()   { TRC("drop ") wdDrop(&fvm.ds); }
+void Swap()   { TRC("swap ") ; }
+void Over()   { TRC("over ") ; }
+void Rot()    { TRC("rot  ") ; }
+void Dup()    { TRC("dup  ") ; }
+void Get()    { TRC("get  ") ; }
+void Put()    { TRC("put  ") ; }
+void Geti()   { TRC("geti ") ; }
+void Puti()   { TRC("puti ") ; }
+void Rom()    { TRC("rom  ") ; }
+void Add()    { TRC("add  ") wdPush(wdPop(&fvm.ds)+wdPop(&fvm.ds),&fvm.ds); } // FIXME
+void Sub()    { TRC("sub  ") ; }
+void Mul()    { TRC("mul  ") ; }
+void Div()    { TRC("div  ") ; }
+void Mod()    { TRC("mod  ") ; }
+void Inc()    { TRC("inc  ") ; }
+void Dec()    { TRC("dec  ") ; }
+void Or()     { TRC("or   ") ; }
+void And()    { TRC("and  ") ; }
+void Xor()    { TRC("xor  ") ; }
+void Flip()   { TRC("flip ") ; }
+void Neg()    { TRC("neg  ") ; }
+void Shl()    { TRC("shl  ") ; }
+void Shr()    { TRC("shr  ") ; }
+void Hold()   { TRC("hold ") ; }
+void Give()   { TRC("give ") ; }
+void In()     { TRC("in   ") wdPush(getchar(), &fvm.ds); }
+void Out()    { TRC("out  ") putchar(wdPop(&fvm.ds)); }
 // Jump
 // Jmpz
 // Jmpe
@@ -249,30 +308,38 @@ void Out()    { putchar(wdPop(&fvm.ds)); }
 // Halt
 // Fail
 // Safe
-void Dsa()    { wdPush(wdElems(&fvm.ds),&fvm.ds); }
-void Dse()    { wdPush(wdFree(&fvm.ds),&fvm.ds); }
-void Tsa()    { wdPush(wdElems(&fvm.ts),&fvm.ds); }
-void Tse()    { wdPush(wdFree(&fvm.ts),&fvm.ds); }
-void Csa()    { wdPush(wdElems(&fvm.cs),&fvm.ds); }
-void Cse()    { wdPush(wdFree(&fvm.cs),&fvm.ds); }
-void Rsa()    { wdPush(natElems(&fvm.rs),&fvm.ds); }
-void Rse()    { wdPush(natFree(&fvm.rs),&fvm.ds); }
-void Pmi()    { wdPush(PMI,&fvm.ds); }
-void Dmw()    { wdPush(DM_WORDS,&fvm.ds); }
-void Rmw()    { wdPush(RM_WORDS,&fvm.ds); }
-void Hw()     { wdPush(HD_WORDS,&fvm.ds); }
-void Tron()   { ; }
-void Troff()  { ; }
+void Dsa()    { TRC("dsa  ") wdPush(wdElems(&fvm.ds),&fvm.ds); }
+void Dse()    { TRC("dse  ") wdPush(wdFree(&fvm.ds),&fvm.ds); }
+void Tsa()    { TRC("tsa  ") wdPush(wdElems(&fvm.ts),&fvm.ds); }
+void Tse()    { TRC("tse  ") wdPush(wdFree(&fvm.ts),&fvm.ds); }
+void Csa()    { TRC("csa  ") wdPush(wdElems(&fvm.cs),&fvm.ds); }
+void Cse()    { TRC("cse  ") wdPush(wdFree(&fvm.cs),&fvm.ds); }
+void Rsa()    { TRC("rsa  ") wdPush(natElems(&fvm.rs),&fvm.ds); }
+void Rse()    { TRC("rse  ") wdPush(natFree(&fvm.rs),&fvm.ds); }
+void Pmi()    { TRC("pmi  ") wdPush(PMI,&fvm.ds); }
+void Dmw()    { TRC("dmw  ") wdPush(DM_WORDS,&fvm.ds); }
+void Rmw()    { TRC("rmw  ") wdPush(RM_WORDS,&fvm.ds); }
+void Hw()     { TRC("hw   ") wdPush(HD_WORDS,&fvm.ds); }
+void Tron()   { TRC("tron ") 
+  #ifdef TRACING_SUPPORTED
+    fvm.tracing = TRUE;
+  #endif
+}
+void Troff()  { TRC("troff") 
+  #ifdef TRACING_SUPPORTED
+    fvm.tracing = FALSE;
+  #endif
+}
 
 // ---------------------------------------------------------------------------
 // Programming language macros
 // ---------------------------------------------------------------------------
 #define noop Noop();
-#define call(label) { \
-  __label__ lr; natPush((NAT)&&lr,&fvm.rs); goto label; lr: ; \
+#define call(label) { __label__ lr; \
+  TRC("call ") natPush((NAT)&&lr,&fvm.rs); goto label; lr: ; \
 }
-#define ret { goto *(natPop(&fvm.rs)); }
-#define rpt(label) if (wdPeekAndDec(&fvm.cs) > 0) { goto label; }
+#define ret { TRC("ret  ") goto *(natPop(&fvm.rs)); }
+#define rpt(label) { TRC("rpt ") if (wdPeekAndDec(&fvm.cs) > 0) goto label; }
 #define cpush Cpush();
 #define cpop Cpop();
 #define cpeek Cpeek();
@@ -311,14 +378,14 @@ void Troff()  { ; }
 #define give Give();
 #define in In();
 #define out Out();
-#define jump(label) goto label;
-#define jmpz(label) if (wdPop(&fvm.ds) == 0) { goto label; }
-#define jmpe(label) if (wdPop(&fvm.ds) == wdPop(&fvm.ds)) { goto label; }
-#define jmpg(label) if (wdPop(&fvm.ds) < wdPop(&fvm.ds)) { goto label; }
-#define jmpl(label) if (wdPop(&fvm.ds) > wdPop(&fvm.ds)) { goto label; }
-#define halt { excode = SUCCESS; return; }
-#define fail { excode = FAILURE; return; }
-#define safe(label) if (wdElems(&fvm.ds) < 2) {goto label; }
+#define jump(label) { TRC("jump ") goto label; }
+#define jmpz(label) { TRC("jmpz ") if (wdPop(&fvm.ds) == 0) goto label; }
+#define jmpe(label) { TRC("jmpe ") if (wdPop(&fvm.ds) == wdPop(&fvm.ds)) goto label; }
+#define jmpg(label) { TRC("jmpg ") if (wdPop(&fvm.ds) < wdPop(&fvm.ds)) goto label; }
+#define jmpl(label) { TRC("jmpl ") if (wdPop(&fvm.ds) > wdPop(&fvm.ds)) goto label; }
+#define halt { TRC("halt ") excode = SUCCESS; return; }
+#define fail { TRC("fail ") excode = FAILURE; return; }
+#define safe(label) { TRC("safe ") if (wdElems(&fvm.ds) < 2) goto label; }
 #define dsa Dsa();
 #define dse Dse();
 #define tsa Tsa();
@@ -338,6 +405,11 @@ void Troff()  { ; }
 // I/O start-up 
 // ---------------------------------------------------------------------------
 int startup(FVM *fvm) {
+  #ifdef TRACING_SUPPORTED
+    fvm->tracing = FALSE;
+    stdtrcHandle = fopen(stdtrcFilename, "w");
+    if (!stdtrcHandle) return FAILURE;
+  #endif
   stdhldHandle = fopen(stdhldFilename, "r+b");
   if (!stdhldHandle) return FAILURE;
   if (fread(fvm->hd,WD_BYTES,HD_WORDS,stdhldHandle) < HD_WORDS) {
@@ -362,6 +434,9 @@ int startup(FVM *fvm) {
 // --------------------------------------------------------------------------
 int shutdown(FVM *fvm) {
   int shutdown = SUCCESS;
+  #ifdef TRACING_SUPPORTED
+    if (fclose(stdtrcHandle) == EOF) shutdown = FAILURE;
+  #endif
   if (fclose(rmHandle) == EOF) shutdown = FAILURE;
   if (fseek(stdhldHandle,0,SEEK_SET) !=0) {
     shutdown = FAILURE;
@@ -398,7 +473,9 @@ int main() {
 // ---------------------------------------------------------------------------
 void exampleProgram() {
 
-jump(x0)
+tron
+call(x0)
+halt
 
 failed:
   fail
@@ -409,9 +486,15 @@ unsafe:
   halt
 
 x0:
+  // safe(unsafe)
+  i(0x11111111)
+  cpush
+  i(0x33333333)
+  tpush
+  i(0x22222222)
   safe(unsafe)
   add
-  halt
+  ret
 
 }
 // ===========================================================================
